02a24f791dba337b5db2d7d2a5be0292d541f154
John Alcher <johnalcherdoloiras@gmail.com>
2019-02-09 13:31:58 +0800

    Customize the features section

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -117,8 +117,10 @@
 <section class="features" id="features">
     <div class="container">
         <div class="section-heading text-center">
-            <h2>Unlimited Features, Unlimited Fun</h2>
-            <p class="text-muted">Check out what you can do with this app theme!</p>
+            <h2>Just enough to get you started.</h2>
+            <p class="text-muted">
+                We sweat out the basics without getting on your way.
+            </p>
             <hr>
         </div>
         <div class="row">
@@ -130,9 +132,6 @@
                                 <!-- Demo image for screen mockup, you can put an image here, some HTML, an animation, video, or anything else! -->
                                 <img src="{{ asset('img/demo-screen-1.jpg') }}" class="img-fluid" alt="">
                             </div>
-                            <div class="button">
-                                <!-- You can hook the "home button" to some JavaScript events or just remove it -->
-                            </div>
                         </div>
                     </div>
                 </div>
@@ -142,32 +141,36 @@
                     <div class="row">
                         <div class="col-lg-6">
                             <div class="feature-item">
-                                <i class="icon-screen-smartphone text-primary"></i>
-                                <h3>Device Mockups</h3>
-                                <p class="text-muted">Ready to use HTML/CSS device mockups, no Photoshop required!</p>
+                                <i class="icon-lock text-primary"></i>
+                                <h3>Authentication</h3>
+                                <p class="text-muted">
+                                    Laravel's <a href="http://laravel.com/docs/authentication">default authentication</a> hooked up with an admin panel.
+                                </p>
                             </div>
                         </div>
                         <div class="col-lg-6">
                             <div class="feature-item">
-                                <i class="icon-camera text-primary"></i>
-                                <h3>Flexible Use</h3>
-                                <p class="text-muted">Put an image, video, animation, or anything else in the screen!</p>
+                                <i class="icon-envelope-letter text-primary"></i>
+                                <h3>Email</h3>
+                                <p class="text-muted">Email verification and password resets out of the box.</p>
                             </div>
                         </div>
                     </div>
                     <div class="row">
                         <div class="col-lg-6">
                             <div class="feature-item">
-                                <i class="icon-present text-primary"></i>
-                                <h3>Free to Use</h3>
-                                <p class="text-muted">As always, this theme is free to download and use for any purpose!</p>
+                                <i class="icon-people text-primary"></i>
+                                <h3>Authorization</h3>
+                                <p class="text-muted">Roles and permissions with no external package required.</p>
                             </div>
                         </div>
                         <div class="col-lg-6">
                             <div class="feature-item">
-                                <i class="icon-lock-open text-primary"></i>
+                                <i class="icon-wallet text-primary"></i>
                                 <h3>Open Source</h3>
-                                <p class="text-muted">Since this theme is MIT licensed, you can use it commercially!</p>
+                                <p class="text-muted">
+                                    Free to use with optional premium support <a href="#">available.</a>
+                                </p>
                             </div>
                         </div>
                     </div>
