f32335c9f37c8319cc09de4e086bcb747b850264
John Alcher <johnalcherdoloiras@gmail.com>
2019-02-11 11:57:45 +0800

    Run make:auth

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+
+Auth::routes();
 
 Route::view('/', 'pages.home')->name('pages.home');
 
+Route::get('/home', 'HomeController@index')->name('home');
