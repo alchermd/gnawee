e30b68cb8d984d8e7f924dd6c29a62cb13a1d7b4
John Alcher <johnalcherdoloiras@gmail.com>
2019-02-12 20:47:45 +0800

    Slice up the footer into its own blade file

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -37,15 +37,7 @@
 
             @yield('content')
 
-            <!-- Footer -->
-            <footer class="sticky-footer bg-white">
-                <div class="container my-auto">
-                    <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; Your Website 2019</span>
-                    </div>
-                </div>
-            </footer>
-            <!-- End of Footer -->
+            @include('layouts.dashboard.footer')
 
             <!-- Scroll to Top Button-->
             <a class="scroll-to-top rounded" href="#page-top">
---
+++ resources/views/layouts/dashboard/footer.blade.php
@@ -0,0 +1,12 @@
+<!-- Footer -->
+<footer class="sticky-footer bg-white">
+    <div class="container my-auto">
+        <div class="copyright text-center my-auto">
+            <span>
+                Copyright &copy;
+                {{ config('app.author') }} {{ now()->year }}
+            </span>
+        </div>
+    </div>
+</footer>
+<!-- End of Footer -->
