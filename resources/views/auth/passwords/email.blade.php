c410c5fea8f8debdafc1d178f002650dc455d7a4
John Alcher <johnalcherdoloiras@gmail.com>
2019-02-22 15:46:49 +0800

    Integrate the password reset functionality

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,68 @@
-@extends('layouts.app')
+@extends('layouts.auth')
+
+@section('title', 'Reset Pasword')
 
 @section('content')
-<div class="container">
+    <!-- Outer Row -->
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+        <div class="col-xl-10 col-lg-12 col-md-9">
 
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
+            <div class="card o-hidden border-0 shadow-lg my-5">
+                <div class="card-body p-0">
+                    <!-- Nested Row within Card Body -->
+                    <div class="row">
+                        <div class="col-lg-6 d-none d-lg-block bg-password-image"></div>
+                        <div class="col-lg-6">
+                            <div class="p-5">
+                                @if (session('status'))
+                                    <div class="alert alert-success" role="alert">
+                                        {{ session('status') }}
+                                    </div>
                                 @endif
-                            </div>
-                        </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
+                                <div class="text-center">
+                                    <h1 class="h4 text-gray-900 mb-2">Forgot Your Password?</h1>
+                                    <p class="mb-4">We get it, stuff happens. Just enter your email address below and
+                                        we'll send you a link to reset your password!</p>
+                                </div>
+
+                                <form class="user" method="POST" action="{{ route('password.email') }}">
+                                    @csrf
+
+                                    <div class="form-group">
+                                        <input class="form-control-user
+                                                    form-control{{ $errors->has('email') ? ' is-invalid' : '' }}"
+                                               id="email" type="email" name="email"
+                                               placeholder="Enter Email Address..."
+                                               value="{{ old('email') }}" required>
+
+                                        @if ($errors->has('email'))
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $errors->first('email') }}</strong>
+                                            </span>
+                                        @endif
+                                    </div>
+
+                                    <input type="submit" class="btn btn-primary btn-user btn-block" value="Submit">
+                                </form>
+
+                                <hr>
+
+                                <div class="text-center">
+                                    <a class="small" href="{{ route('register') }}">Create an Account!</a>
+                                </div>
+
+                                <div class="text-center">
+                                    <a class="small" href="{{ route('login') }}">Already have an account? Login!</a>
+                                </div>
                             </div>
                         </div>
-                    </form>
+                    </div>
                 </div>
             </div>
+
         </div>
+
     </div>
-</div>
 @endsection
